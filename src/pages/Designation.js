8d294c56a2c07475dd7f01e05717a8428fd663bf
it21155352 <it21155352@my.sliit.lk>
2024-04-01 10:20:14 +0530

    deplye

--- src/pages/Designation.js
+++ src/pages/Designation.js
@@ -23,7 +23,7 @@ function Designation() {
            <img src='https://cdn.wallpapersafari.com/68/2/MShsNr.jpg' className=' w-[900px] ' />
            <div className='lg:absolute h-[350px]   -right-[75px]  bottom-12   lg:bg-slate-50 sm:bg-white w-[400px]   drop-shadow-2xl'>
                <div className=' py-2 px-2 ml-9'>
-                        <h1 className=' text-3xl mt-7 text-bn-cl'>Cinnamon Dhonveli Maldives</h1>
+                        <h1 className=' text-3xl mt-7 text-bn-cl'>AJ cinone Dhonveli Maldives</h1>
                         <p className=' mt-6 mr-4  text-black '>Feel an irrepressible wave of excitement as you have the best surfing experience, set to the backdrop of serenity, and pristine blue waters.</p>
                         <Btton sty={' py-2 px-5  bg-blue-950 text-white mt-5 '}  name={'Discover More'}/>
                </div>
